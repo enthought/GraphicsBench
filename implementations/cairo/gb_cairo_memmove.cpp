31c45e187a36181d2b292236f0f7512a53cc7253
Deepankar Sharma <deepankarsharma@gmail.com>
2012-04-03 23:32:23 -0400

    Added partial support for Qt gui

--- implementations/cairo/gb_cairo_memmove.cpp
+++ implementations/cairo/gb_cairo_memmove.cpp
@@ -12,11 +12,59 @@
 #include <sstream>
 #include <string.h>
 
+#include <QApplication>
+#include <QPainter>
+
 using std::ifstream;
 using std::ios_base;
 using std::cout;
 using std::cin;
 using std::endl;
+
+
+CairoQTWidget::CairoQTWidget(QWidget *parent)
+    : QWidget(parent)
+{
+
+}
+
+void CairoQTWidget::paintEvent(QPaintEvent *event)
+{
+
+  QPen pen(Qt::black, 2, Qt::SolidLine);
+
+  QPainter painter(this);
+
+  painter.setPen(pen);
+  painter.drawLine(20, 40, 250, 40);
+
+  pen.setStyle(Qt::DashLine);
+  painter.setPen(pen);
+  painter.drawLine(20, 80, 250, 80);
+
+  pen.setStyle(Qt::DashDotLine);
+  painter.setPen(pen);
+  painter.drawLine(20, 120, 250, 120);
+
+  pen.setStyle(Qt::DotLine);
+  painter.setPen(pen);
+  painter.drawLine(20, 160, 250, 160);
+
+  pen.setStyle(Qt::DashDotDotLine);
+  painter.setPen(pen);
+  painter.drawLine(20, 200, 250, 200);
+
+
+  QVector<qreal> dashes;
+  qreal space = 4;
+
+  dashes << 1 << space << 5 << space;
+
+  pen.setStyle(Qt::CustomDashLine);
+  pen.setDashPattern(dashes);
+  painter.setPen(pen);
+  painter.drawLine(20, 240, 250, 240);
+}
 
 
 void GBCairoMemmoveBenchmark::init()
--- implementations/cairo/gb_cairo_memmove.h
+++ implementations/cairo/gb_cairo_memmove.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#pragma once
-
 #include "gb_cairo_benchmark.h"
 #include <gb_box.h>
+#include <QWidget>
+
 
 class GBCairoMemmoveBenchmark : public GBBenchmark<GBBoxMeta, GBBoxData, CairoContext>
 {
@@ -11,4 +11,16 @@ public:
     GBBoxAppState app_state;
     virtual void init();
     virtual void render();
+};
+
+class CairoQTWidget : public QWidget
+{
+  Q_OBJECT  
+
+  public:
+    CairoQTWidget(QWidget *parent = 0);
+
+  protected:
+    void paintEvent(QPaintEvent *event);
+
 };
